c347f725019653fe8a3fa0f37fef339b703b85b6
stepnicky <adamstepnicky@gmail.com>
2025-07-27 23:00:10 +0200

    fix init.sql

--- db-init/init.sql
+++ db-init/init.sql
@@ -24,7 +24,7 @@ INSERT INTO cast_me.actors (id, education, agency_id, feature_set_id, user_id) V
 INSERT INTO cast_me.skills (id, name) VALUES (1, 'Horse riding');
 INSERT INTO cast_me.skills (id, name) VALUES (2, 'Dancing');
 INSERT INTO cast_me.skills (id, name) VALUES (3, 'Singing');
-INSERT INTO cast_me.skills (id, name) VALUES (4, `Driver's license`);
+INSERT INTO cast_me.skills (id, name) VALUES (4, 'Driver\'s license');
 INSERT INTO cast_me.skills (id, name) VALUES (5, 'Running really fast');
 INSERT INTO cast_me.skills (id, name) VALUES (6, 'Jumping');
 INSERT INTO cast_me.skills (id, name) VALUES (7, 'Melting stuff');
@@ -43,12 +43,12 @@ INSERT INTO cast_me.actors_skills (actor_id, skills_id) VALUES (4, 4);
 INSERT INTO cast_me.actors_skills (actor_id, skills_id) VALUES (4, 5);
 INSERT INTO cast_me.actors_skills (actor_id, skills_id) VALUES (4, 7);
 
+INSERT INTO cast_me.castings (id, created_on, deadline, description, is_active, title, casting_director_id) VALUES (1, '2025-07-27', '2025-08-15', 'We are seeking diverse individuals of all ages, backgrounds, and experience levels! Any accent, look, or personality – diversity is key!', true, 'Actors & Actresses of All Ages Wanted for Upcoming Production!', 1);
+
+INSERT INTO cast_me.roles (id, description, title, feature_set_id, casting_id) VALUES (1, 'Bright-eyed, imaginative, and always asking questions. Loves animals and carries a notebook everywhere to write down "important discoveries." Must be expressive and comfortable speaking on camera.', 'Charles', 5, 1);
+INSERT INTO cast_me.roles (id, description, title, feature_set_id, casting_id) VALUES (2, 'Introverted and intelligent, with a mysterious side. Carries the weight of something unspoken. Strong non-verbal acting skills required to convey emotion through subtlety.', 'Emma', 6, 1);
+
 INSERT INTO cast_me.actors_roles (id, actor_id, role_id) VALUES (1, 2, 1);
 INSERT INTO cast_me.actors_roles (id, actor_id, role_id) VALUES (2, 4, 1);
 INSERT INTO cast_me.actors_roles (id, actor_id, role_id) VALUES (3, 1, 2);
 INSERT INTO cast_me.actors_roles (id, actor_id, role_id) VALUES (4, 3, 2);
-
-INSERT INTO cast_me.castings (id, created_on, deadline, description, is_active, title, casting_director_id) VALUES (1, '2025-07-27', '2025-08-15', "We are seeking diverse individuals of all ages, backgrounds, and experience levels. Whether you\'re a seasoned actor or brand new to performing, we’d love to see what you’ve got! Any accent, look, or personality – diversity is key!", true, 'Actors & Actresses of All Ages Wanted for Upcoming Production!', 1);
-
-INSERT INTO cast_me.roles (id, description, title, feature_set_id, casting_id) VALUES (1, 'Bright-eyed, imaginative, and always asking questions. Loves animals and carries a notebook everywhere to write down "important discoveries." Must be expressive and comfortable speaking on camera.', 'Charles', 5, 1);
-INSERT INTO cast_me.roles (id, description, title, feature_set_id, casting_id) VALUES (2, 'Introverted and intelligent, with a mysterious side. Carries the weight of something unspoken. Strong non-verbal acting skills required to convey emotion through subtlety.', 'Emma', 6, 1);
